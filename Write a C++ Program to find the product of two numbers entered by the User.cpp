182a6824d36ae51099f88c5d19e4c6b0b5ccb4c3
Nishu Gupta <101319915+NishuGuptaa@users.noreply.github.com>
2022-03-12 18:18:12 +0530

    Add files via upload

---
+++ Write a C++ Program to find the product of two numbers entered by the User.cpp
@@ -0,0 +1,13 @@
+//2.Write a C++ Program to find the product of two numbers entered by the User
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	double n1,n2,product;
+	cout<<"Enter two numbers: ";
+	cin>>n1>>n2;
+	product=n1*n2;
+	cout<<"Product = "<<product;
+	return 0;
+}
